79aba7a99ae98b4c6a7bd4136e4e7e65a9b98ceb
Joshua E. Rice <j.e.rice4101@gmail.com>
2024-04-23 15:55:18 -0400

    Add files via upload

--- MyNavBar.js
+++ MyNavBar.js
@@ -2,7 +2,8 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import { NavLink } from 'react-router-dom';
+
 
 // Define MyNavBar component
 function MyNavBar() {
@@ -18,23 +19,11 @@ function MyNavBar() {
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             {/* Navigation Links */}
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            {/* NavDropdown */}
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              {/* Dropdown Items */}
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              {/* Dropdown Divider */}
-              <NavDropdown.Divider />
-              {/* Dropdown Item */}
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+            <Nav.Link as={NavLink} to='/'>Home</Nav.Link>
+            <Nav.Link as={NavLink} to='/Blog'>Blog</Nav.Link>
+            <Nav.Link as={NavLink} to='/About'>About</Nav.Link>
+            <Nav.Link as={NavLink} to='/Products'>Products</Nav.Link>
+            <Nav.Link as={NavLink} to='/MyTable'>My Table</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
