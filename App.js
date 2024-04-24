1f6f71f25c690946e40e0fd09aa561bf5667842d
Joshua E. Rice <j.e.rice4101@gmail.com>
2024-04-24 00:15:38 -0400

    Add files via upload
    Learning more use of useState functionality by creating a "delete" mechansim.

---
+++ App.js
@@ -0,0 +1,93 @@
+import React, { useState } from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MyNavBar from "./components/MyNavbar";
+import HomePage from "./pages/HomePage";
+import ProductsPage from "./pages/ProductsPage";
+import AboutPage from "./pages/AboutPage";
+import BlogPage from "./pages/BlogPage";
+import MyTable from "./pages/MyTable";
+import MyArticle from "./components/MyArticle";
+import Test from './pages/test';
+
+function App() {
+  // State variables for visibility of each section
+  const [isVisible, setIsVisible] = useState([true, true, true, true, true, true, true, true, true]);
+
+  // State variable for dynamic text
+  const [myText, setMyText] = useState('Joshua Rice');
+
+  // State variable for demonstration of useState with different types
+  const [myNum, setMyNum] = useState(1);
+
+  // Function to toggle visibility of a section based on index
+  const toggleVisibility = (index) => {
+    // Create a copy of isVisible array
+    const updatedVisibility = [...isVisible];
+    // Toggle the visibility of the section at the given index
+    updatedVisibility[index] = !updatedVisibility[index];
+    // Update the state with the new visibility array
+    setIsVisible(updatedVisibility);
+  };
+
+  // Return JSX markup to render the application UI
+  return (
+    <>
+      <BrowserRouter>
+        <MyNavBar />
+        {/* Define routes for different pages */}
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/Blog' element={<BlogPage />} />
+          <Route path='/Products' element={<ProductsPage />} />
+          <Route path='/About' element={<AboutPage />} />
+          <Route path='/MyTable' element={<MyTable />} />
+          <Route path='/Test' element={<Test />} />
+        </Routes>
+      </BrowserRouter>
+
+      {/* Button and section for React Components / Props lesson 1 & 2 */}
+      <button onClick={() => toggleVisibility(0)}>React Components / Props lesson 1 & 2</button>
+      {isVisible[0] && (
+        <div>
+          <h1>React Components / Props lesson 1 & 2</h1>
+          <MyArticle
+            title="My First Article"
+            paragraph="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum quidem eos quasi libero sed debitis blanditiis culpa, laudantium nesciunt aliquam atque explicabo alias temporibus amet ullam ducimus suscipit sit porro!"
+          />
+        </div>
+      )}
+
+      {/* Button and section for React UseState */}
+      <button onClick={() => toggleVisibility(1)}>React UseState</button>
+      {isVisible[1] && (
+        <div>
+          <h1>React UseState</h1>
+          <button onClick={() => toggleVisibility(6)}>Click Me!</button>
+          {isVisible[6] && (<h2>Hello World</h2>)}
+        </div>
+      )}
+
+      {/* Button and section for Mastering Syntax */}
+      <button onClick={() => toggleVisibility(2)}>Mastering Syntax: Working the Function as Second the Parameter of useState Hook</button>
+      {isVisible[2] && (
+        <div>
+          <h1>Mastering Syntax: Working the Function as Second the Parameter of useState Hook</h1>
+          <h2>{myText}</h2>
+          <button onClick={() => setMyText('Sarah Rice')}>Click Me!</button>
+        </div>
+      )}
+
+      {/* Section for Plain JavaScript VS. React.js */}
+      <button onClick={() => toggleVisibility(5)}>Plain JavaScript VS. React.js: Working with Variables</button>
+      {isVisible[5] && 
+        <div>
+          <h1>Plain JavaScript VS. React.js: Working with Variables (Numbers)</h1>
+          <button onClick={() => setMyNum(myNum * 2)}>Click Me!</button>
+          <h2>{myNum}</h2>
+        </div>
+      }
+    </>
+  );
+}
+
+export default App;
--- pages/MyTable.js
+++ pages/MyTable.js
@@ -2,35 +2,38 @@
 import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Table from 'react-bootstrap/Table';
+import { Container, Form, Row, Col } from 'react-bootstrap';
 
 // Define MyTable component
 function MyTable(){
 
     // Define an array of user data
-    const users=[
-
+    const [users , setUsers] = useState([
         {
             fname: "Joshua",
             lname: "Rice",
-            city: "Birdsboro"
+            city: "Ephrata",
+            age: 36
         },
         {
             fname: "Sarah",
             lname: "Beiler",
-            city: "Lancaster"
+            city: "Narvon",
+            age: 35
         },
         {
             fname: "Ava",
-            lname: "Andrew",
-            city: "Reading"
+            lname: "Rice",
+            city: "Reading",
+            age: 6
         },
         {
             fname: "Adeline",
-            lname: "Jenks",
-            city: "Oley"
+            lname: "Rice",
+            city: "Wyomissing",
+            age: 2
         }
-
-    ]
+    ])
 
     // Define state variable for search key
     const [searchKey, setSearchKey] = useState('');
@@ -46,41 +49,84 @@ function MyTable(){
         return newCriteria.toLocaleLowerCase().includes(searchKey.toLocaleLowerCase());
     })
 
+    // Function to handle click event for test button
+    const testClick = () => {
+        alert('Button Is Working');
+    }
+
+    // Function to handle delete operation for a user
+    const handleDelete = (i) => {
+        let userCopy = [...users];
+        userCopy.splice(i, 1);
+        setUsers(userCopy);
+    }
+
     // Render JSX markup to display table of users
     return (
         <>
-            <h1>My Table Page</h1>
-            <h2>List of Users</h2>
-            {/* Input field for search */}
-            <input type="text" name="" id="" placeholder='Search' onChange={inputChange}/>
-            {/* Table to display user data */}
-            <Table striped>
-                {/* Table header */}
-                <thead className='table-dark'>
-                    <tr>
-                        <th>First Name</th>
-                        <th>Last Name</th>
-                        <th>City</th>
-                        <th>Operation</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {/* Map over filtered users and render each user row */}
-                    {filteredUsers.map((user, index) => (
-                        <tr key={index}>
-                            <td>{user.fname}</td>
-                            <td>{user.lname}</td>
-                            <td>{user.city}</td>
-                            <td>
-                                {/* Buttons for edit, update, and delete operations */}
-                                <Button variant="warning mx-2" size='sm'>Edit</Button>
-                                <Button variant="info mx-2" size='sm'>Update</Button>
-                                <Button variant="danger mx-2" size='sm'>Delete</Button>
-                            </td>
-                        </tr>
-                    ))}           
-                </tbody>
-            </Table>
+            <Container>
+                <Row>
+                    {/* Form for adding new user */}
+                    <Col md={4}>
+                        <Form>
+                            <Form.Group className="mb-3">
+                                <Form.Label>Enter First Name: </Form.Label>
+                                <Form.Control type="test" placeholder="Enter First Name" />
+                            </Form.Group>
+                            <Form.Group className="mb-3">
+                                <Form.Label>Enter Last Name: </Form.Label>
+                                <Form.Control type="text" placeholder="Enter Last Name" />
+                            </Form.Group>
+                            <Form.Group className="mb-3">
+                                <Form.Label>Enter City: </Form.Label>
+                                <Form.Control type="text" placeholder="Enter City" />
+                            </Form.Group>
+                            <Form.Group className="mb-3">
+                                <Form.Label>Enter Age : </Form.Label>
+                                <Form.Control type="number" placeholder="Enter Age" />
+                            </Form.Group>
+                            <Form.Group className="mb-3">
+                                <Button variant="primary" onClick={testClick}>Submit</Button>
+                            </Form.Group>
+                        </Form>
+                    </Col>
+                    {/* Display table of users */}
+                    <Col md={8}>
+                        <h1>My Table Page</h1>
+                        <h2>List of Users</h2>
+                        {/* Input field for search */}
+                        <input  className="mb-4" type="text" name="" id="" placeholder='Search' onChange={inputChange}/>
+                        {/* Table to display user data */}
+                        <Table striped>
+                            {/* Table header */}
+                            <thead className='table-dark'>
+                                <tr>
+                                    <th>First Name</th>
+                                    <th>Last Name</th>
+                                    <th>City</th>
+                                    <th>Operation</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {/* Map over filtered users and render each user row */}
+                                {filteredUsers.map((user, index) => (
+                                    <tr key={index}>
+                                        <td>{user.fname}</td>
+                                        <td>{user.lname}</td>
+                                        <td>{user.city}</td>
+                                        <td>
+                                            {/* Buttons for edit, update, and delete operations */}
+                                            <Button variant="warning mx-2" size='sm'>Edit</Button>
+                                            <Button variant="info mx-2" size='sm'>Update</Button>
+                                            <Button variant="danger mx-2" size='sm' onClick={() => handleDelete(index)}>Delete</Button>
+                                        </td>
+                                    </tr>
+                                ))}           
+                            </tbody>
+                        </Table>
+                    </Col>
+                </Row>
+            </Container>
         </>
     );
 }
