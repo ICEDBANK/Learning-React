18fbbccb217bf3b10f75119932ab5f813e12ce7f
ICEDBANK <j.e.rice4101@gmail.com>
2024-04-25 00:19:34 -0400

    Automated commit

--- pages/HomePage.js
+++ pages/HomePage.js
@@ -7,10 +7,7 @@ function HomePage(){
             <h1>Home</h1>
             <h2>Testting...</h2>
             <h2>Testting...</h2>
-            <h2>Testting...</h2>
-            <h2>Testting...</h2>
-            <h2>Testting...</h2>
-            <h2>Testting...</h2>
+            
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea a nisi quos quisquam iure ducimus iusto officia nihil et illo ratione maiores sit qui, vel eius fugit, quidem sequi modi!</p>
             
         </>
