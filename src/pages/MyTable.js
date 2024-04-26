99ceff0b2b4cae3a8fa316181a3af64f006fe6f1
ICEDBANK <j.e.rice4101@gmail.com>
2024-04-25 22:44:17 -0400

    Automated commit

--- src/pages/MyTable.js
+++ src/pages/MyTable.js
@@ -111,7 +111,7 @@ function MyTable(){
         
         event.preventDefault();
         let usersCopy = [...users];
-        let targetItem = usersCopy
+        let targetItem = usersCopy;
         setEditing(true);
 
     }
