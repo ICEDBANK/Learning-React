0a47fd0c86fb3d76d46a761ec2514e226e8033b6
ICEDBANK <j.e.rice4101@gmail.com>
2024-04-26 14:21:08 -0400

    Automated commit

--- pages/MyTable.js
+++ pages/MyTable.js
@@ -106,7 +106,7 @@ function MyTable(){
         setLocation(targetItem.city);
         setCurrentAge(targetItem.age);
         settargetId(index);
-        console.log({targetItem});
+        console.log('edit clicked');
         setEditing(false);
     };
 
